d97be60a8ff12d2553c1c1b75197630335f544fc
aliaksandr <sspvitebsk@gmail.com>
2026-01-25 23:19:19 +0300

    Обновление

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -10,31 +10,18 @@ import java.time.Duration;
  * Базовый класс для настройки и управления тестовым окружением WebDriver.
  */
 public class TestBase {
-    /**
-     * Экземпляр WebDriver для управления браузером.
-     */
     protected WebDriver driver;
 
-    /**
-     * Настраивает и инициализирует тестовое окружение.
-     * Устанавливает драйвер Chrome
-     * Максимизирует окно браузера
-     * Устанавливает неявное ожидание.
-     */
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        WebDriverManager.chromedriver().setup(); //Настраивает и инициализирует тестовое окружение.
+        driver = new ChromeDriver(); //Устанавливает драйвер Chrome
+        driver.manage().window().maximize(); //Максимизирует окно браузера
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(TestData.DEFAULT_TIMEOUT)); //Устанавливает неявное ожидание.
     }
 
-    /**
-     * Завершает работу тестового окружения.
-     * Закрывает браузер и освобождает ресурсы WebDriver, если он был инициализирован.
-     */
     public void tearDown (){
         if (driver != null) {
             driver.quit();
-        }
+        } //Закрывает браузер и освобождает ресурсы WebDriver, если он был инициализирован.
     }
 }
