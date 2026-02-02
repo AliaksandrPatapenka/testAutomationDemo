0ae8d9685024c9e74190ed2076df58d78b463e09
AliaksandrPatapenka <sspvitebsk@gmail.com>
2026-02-02 13:59:32 +0300

    Обновление

--- src/test/java/helpers/AuthHelper.java
+++ src/test/java/helpers/AuthHelper.java
@@ -1,19 +1,51 @@
 package helpers;
 
 import base.TestBase;
+import base.TestData;
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.devtools.v120.network.model.Cookie;
-
+import page.LoginPage;
+import org.openqa.selenium.Cookie;
 import java.util.Set;
+import org.openqa.selenium.JavascriptExecutor;
 
 public class AuthHelper {
     private AuthHelper(){} //Приватный конструктор - чтобы нельзя было создать объект этого класса. Все методы будут статическими, вызываем как AuthHelper.methodName()
 
-    public static Set<Cookie> getAuthCookie () {
+    public static Set<Cookie> getAuthCookie (String expectedText) {
         TestBase testBase = new TestBase();
         testBase.setUp();
-        return Cookies;
+        WebDriver driver = testBase.getDriver(); // Создаем экземпляр драйвера для TestBase
+        driver.manage().deleteAllCookies();
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.openPageLogin();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        loginPage.login(TestData.USER_LOGIN, TestData.USER_PASSWORD);
+        //String actualText;
+        //actualText = loginPage.getAutSuccess();
+        //Assertions.assertEquals(expectedText, actualText, "Нет нужного текста");
+        String token = (String) ((org.openqa.selenium.JavascriptExecutor) driver).executeScript("return JSON.parse(localStorage.getItem('prop.sessionEntity')).token");
+        Cookie sessionCookie = new Cookie.Builder("session", token).domain("localhost").path("/").isHttpOnly(true).build();
+        driver.manage().addCookie(sessionCookie);
+        Set<Cookie> allCookie = driver.manage().getCookies();
+            for (Cookie cookie : allCookie) {
+                System.out.println("Имя: " + cookie.getName());
+                System.out.println("Значение: " + cookie.getValue());
+            }// Выводим все сыормированные Cookie
+
+       // Set<Cookie> testCookie = filterCookies(allCookie);
+        //testBase.tearDown();
+        return allCookie;
+
+
     }
 
-
+    public static void main(String[] args) {
+        getAuthCookie(TestData.EXPECTED_TEXT_AUTH);
+    }
 }
