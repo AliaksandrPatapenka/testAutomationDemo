3704ec2cfd09f4107cc3cef1d01d6edaad6935dd
AliaksandrPatapenka <sspvitebsk@gmail.com>
2026-02-03 14:26:10 +0300

    Обновление

--- src/test/java/base/TestData.java
+++ src/test/java/base/TestData.java
@@ -5,7 +5,9 @@ package base;
  */
 public final class TestData {
 
-    public static final String BASE_URL = System.getProperty("base.url", "http://localhost:5001");
+    public static final String BASE_DOMAIN = System.getProperty("base.domain", "localhost");//было BASE_URL
+    public static final String BASE_PROTOCOL = System.getProperty("base.protocol", "http://");
+    public static final String BASE_PORT = System.getProperty("base.port", ":5001");
     public static final String LOGIN_URL = System.getProperty("login.url", "/login");
 
     public static final String USER_LOGIN = System.getProperty("user.login", "user");
