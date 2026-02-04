376d9f7368081c18a51a44518f949cb3312c3b07
AliaksandrPatapenka <sspvitebsk@gmail.com>
2026-02-04 09:09:48 +0300

    Обновление

--- src/test/java/helpers/AuthHelper.java
+++ src/test/java/helpers/AuthHelper.java
@@ -13,7 +13,7 @@ public class AuthHelper {
     private AuthHelper(){} //Приватный конструктор - чтобы нельзя было создать объект этого класса. Все методы будут статическими, вызываем как AuthHelper.methodName()
     private static final Logger log = LoggerFactory.getLogger(AuthHelper.class);
 
-    public static Set<Cookie> getAuthCookie () {
+    public static void getAuthCookie () {
         TestBase testBase = new TestBase();
 
         testBase.setUp();
@@ -28,15 +28,15 @@ public class AuthHelper {
             Thread.currentThread().interrupt();
             throw new RuntimeException("Thread was interrupted", e);
         }
-        String token = (String) ((org.openqa.selenium.JavascriptExecutor) driver).executeScript("return JSON.parse(localStorage.getItem('prop.sessionEntity')).token");
+
+        String token = (String) ((org.openqa.selenium.JavascriptExecutor) driver).executeScript("return JSON.parse(localStorage.getItem('prop.sessionEntity')).token");//Получение токена из LocalStorage
         Cookie sessionCookie = new Cookie.Builder("session", token)
                 .domain(TestData.BASE_DOMAIN).path("/")
-                .isHttpOnly(true).build();
-        driver.manage().addCookie(sessionCookie);
-        Set<Cookie> userCookie = driver.manage().getCookies();
-        log.info("session cookie = {}",token);
-
-        return userCookie;
+               .isHttpOnly(true).build();//Создание из токена куки
+        driver.manage().addCookie(sessionCookie);//Добавление куки в браузер
+        Set<Cookie> allCookie = driver.manage().getCookies();//Получение всех кук
+        log.info("Session cookie = {}",sessionCookie);
+        log.info("Все cookie = {}",allCookie);
     }
 
     public static void main(String[] args) {
