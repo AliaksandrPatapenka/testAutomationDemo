b5da8d84518f5f3f8faa90f68714e8746d9fc6c2
AliaksandrPatapenka <sspvitebsk@gmail.com>
2026-02-20 18:55:34 +0300

    Обновление

--- src/test/java/helpers/AuthHelper.java
+++ src/test/java/helpers/AuthHelper.java
@@ -6,7 +6,6 @@ import page.LoginPage;
 public class AuthHelper {
     public static void login(WebDriver driver, String login, String password) {
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.openPageLogin();
         loginPage.login(login,  password);
     }
 }
