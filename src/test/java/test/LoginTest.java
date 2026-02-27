9a7369b4aeb7adf853faf6679727b044eb515e1f
AliaksandrPatapenka <sspvitebsk@gmail.com>
2026-02-27 15:54:15 +0300

    Обновление

--- src/test/java/test/LoginTest.java
+++ src/test/java/test/LoginTest.java
@@ -24,7 +24,6 @@ public class LoginTest extends TestBase {
     public void setUpTest(){
         setUp();
         loginPage = new LoginPage(driver);
-        loginPage.openPageLogin();
     }
 
     @AfterEach
